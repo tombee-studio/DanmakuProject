d796d3ac14e2c9f1d28fd30adceb574f79b749bb
Appbird <project.appbird@gmail.com>
2022-08-03 17:45:53 +0900

    Je and Jne comes to get the peek instead of popping from the stack.

--- Assets/Scripts/EnemyInterpreterComponents/EnemyVM/InstructionSet/Jump.cs
+++ Assets/Scripts/EnemyInterpreterComponents/EnemyVM/InstructionSet/Jump.cs
@@ -7,10 +7,10 @@ public partial class EnemyVM
     }
     private void Je(Instruction instruction)
     {
-        if (PopFromStack() != 0) programCounter = instruction.argument;
+        if (Peek() != 0) programCounter = instruction.argument;
     }
     private void Jne(Instruction instruction)
     {
-        if (PopFromStack() == 0) programCounter = instruction.argument;
+        if (Peek() == 0) programCounter = instruction.argument;
     }
 }

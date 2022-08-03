059f55009ac4362ac3e82a746b0f62a72d2a0276
Appbird <project.appbird@gmail.com>
2022-08-03 23:54:41 +0900

    Define Tester

---
+++ Assets/Scripts/EnemyInterpreterComponents/EnemyVM/Tester/Arithmetic.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine.Assertions;
+
+public partial class EnemyVMTester
+{
+    public void test_ADD()
+    {
+        vm.appendInstruction(
+            new EnemyVM.Instruction(EnemyVM.Mnemonic.PUSH, 2)
+        );
+        vm.appendInstruction(
+            new EnemyVM.Instruction(EnemyVM.Mnemonic.PUSH, 9)
+        );
+        vm.appendInstruction(
+            new EnemyVM.Instruction(EnemyVM.Mnemonic.ADD, 0)
+        );
+        while (!IsExit) run();
+        Assert.AreEqual(11, vm.ReturnValue);
+    }
+    public void test_SUB()
+    {
+        vm.appendInstruction(
+            new EnemyVM.Instruction(EnemyVM.Mnemonic.PUSH, 2)
+        );
+        vm.appendInstruction(
+            new EnemyVM.Instruction(EnemyVM.Mnemonic.PUSH, 9)
+        );
+        vm.appendInstruction(
+            new EnemyVM.Instruction(EnemyVM.Mnemonic.SUB, 0)
+        );
+        while (!IsExit) run();
+        Assert.AreEqual(7, vm.ReturnValue);
+    }
+    public void test_MUL()
+    {
+        vm.appendInstruction(
+            new EnemyVM.Instruction(EnemyVM.Mnemonic.PUSH, 2)
+        );
+        vm.appendInstruction(
+            new EnemyVM.Instruction(EnemyVM.Mnemonic.PUSH, 9)
+        );
+        vm.appendInstruction(
+            new EnemyVM.Instruction(EnemyVM.Mnemonic.MUL, 0)
+        );
+        while (!IsExit) run();
+        Assert.AreEqual(18, vm.ReturnValue);
+    }
+    public void test_DIV1()
+    {
+        vm.appendInstruction(
+            new EnemyVM.Instruction(EnemyVM.Mnemonic.PUSH, 2)
+        );
+        vm.appendInstruction(
+            new EnemyVM.Instruction(EnemyVM.Mnemonic.PUSH, 18)
+        );
+        vm.appendInstruction(
+            new EnemyVM.Instruction(EnemyVM.Mnemonic.DIV, 0)
+        );
+        while (!IsExit) run();
+        Assert.AreEqual(9, vm.ReturnValue);
+    }
+    /** 整数除算がなされるかテスト */
+    public void test_DIV2()
+    {
+        vm.appendInstruction(
+            new EnemyVM.Instruction(EnemyVM.Mnemonic.PUSH, 2)
+        );
+        vm.appendInstruction(
+            new EnemyVM.Instruction(EnemyVM.Mnemonic.PUSH, 7)
+        );
+        vm.appendInstruction(
+            new EnemyVM.Instruction(EnemyVM.Mnemonic.DIV, 0)
+        );
+        while (!IsExit) run();
+        Assert.AreEqual(3, vm.ReturnValue);
+    }
+}
---
+++ Assets/Scripts/EnemyInterpreterComponents/EnemyVM/Tester/EnemyVMTester.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Reflection;
+using System.Text.RegularExpressions;
+using UnityEngine.Assertions;
+using UnityEngine;
+using System.Collections.Generic;
+
+public partial class EnemyVMTester
+{
+
+    public EnemyVM vm = new EnemyVM();
+    public bool IsContinue { get => vm.IsContinute; }
+    public bool IsExit { get => vm.IsExit; }
+    public int ReturnValue { get => vm.ReturnValue; }
+
+    private void run()
+    {
+        vm.run();
+    }
+    private MethodInfo[] selectTestMethods(MethodInfo[] methods)
+    {
+        var testMethods = new List<MethodInfo>();
+        foreach (var method in methods)
+        {
+            if (!Regex.IsMatch(method.Name, "test_")) continue;
+            testMethods.Add(method);
+        }
+
+        return testMethods.ToArray();
+
+    }
+    private MethodInfo[] pickupTestMethods()
+    {
+        BindingFlags flag =
+            BindingFlags.Public |
+            BindingFlags.Instance |
+            BindingFlags.Static |
+            BindingFlags.FlattenHierarchy |
+            BindingFlags.NonPublic;
+        MethodInfo[] methods = GetType().GetMethods(flag);
+        return selectTestMethods(methods);
+    }
+    public void runTests()
+    {
+        foreach (MethodInfo method in pickupTestMethods())
+        {
+            var target = new EnemyVMTester();
+            object[] parametersArray = new object[] { };
+            method.Invoke(target, parametersArray);
+        }
+    }
+    
+}

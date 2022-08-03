1103c4ac30ef628b2cfe38fffc8f4ec0e131a580
Appbird <project.appbird@gmail.com>
2022-08-03 18:07:23 +0900

    reverse operands in Sub and Div.

--- Assets/Scripts/EnemyInterpreterComponents/EnemyVM/InstructionSet/Arithmetic.cs
+++ Assets/Scripts/EnemyInterpreterComponents/EnemyVM/InstructionSet/Arithmetic.cs
@@ -4,8 +4,8 @@ public partial class EnemyVM
 
     private void Add()
     {
-        int operand2 = PopFromStack();
         int operand1 = PopFromStack();
+        int operand2 = PopFromStack();
         PushIntoStack(operand1 + operand2);
     }
 
@@ -13,22 +13,22 @@ public partial class EnemyVM
     {
         // スタックマシン{push ope1, push ope2, MUL} => push ope1 - ope2
         // スタックからPopされる、オペランドの順番はope2, ope1である。
-        int operand2 = PopFromStack();
         int operand1 = PopFromStack();
+        int operand2 = PopFromStack();
         PushIntoStack(operand1 - operand2);
     }
 
     private void Mul()
     {
-        int operand2 = PopFromStack();
         int operand1 = PopFromStack();
+        int operand2 = PopFromStack();
         PushIntoStack(operand1 * operand2);
     }
 
     private void Div()
     {
-        int operand2 = PopFromStack();
         int operand1 = PopFromStack();
+        int operand2 = PopFromStack();
         PushIntoStack(operand1 / operand2);
     }
 }

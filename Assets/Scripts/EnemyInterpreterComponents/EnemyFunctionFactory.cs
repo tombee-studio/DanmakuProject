590ef8bc73d36e3bfa97a53edda846e8a862a47f
Appbird <project.appbird@gmail.com>
2022-08-17 12:59:45 +0900

    modify EnemyFunctionFactory in singleton pattern.

--- Assets/Scripts/EnemyInterpreterComponents/EnemyFunctionFactory.cs
+++ Assets/Scripts/EnemyInterpreterComponents/EnemyFunctionFactory.cs
@@ -1,11 +1,20 @@
 ﻿using System;
-public static class EnemyFunctionFactory
+public class EnemyFunctionFactory
 {
-   public static void Call(
-       int functionCode,
-       EnemyComponent enemyComponent,
-       EnemyVM enemyVM
-   ){
+    private EnemyFunctionFactory(){}
+
+    private static EnemyFunctionFactory _singleton;
+    private static EnemyFunctionFactory singleton {
+        get => _singleton ??= new EnemyFunctionFactory();
+    }
+
+    public static EnemyFunctionFactory GetInstance() => _singleton;
+
+    public void Call(
+        int functionCode,
+        EnemyComponent enemyComponent,
+        EnemyVM enemyVM
+    ){
         throw new Exception("Not implemented.");
-   }
+    }
 }

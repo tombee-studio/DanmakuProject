8fac1d08380e3da5f7515094b41a9fdb375f4f6c
Appbird <project.appbird@gmail.com>
2022-08-31 19:31:55 +0900

    write easy testcases.

--- Assets/Scripts/EnemyInterpreterComponents/EnemyLexer/Tester/SimpleCase.cs
+++ Assets/Scripts/EnemyInterpreterComponents/EnemyLexer/Tester/SimpleCase.cs
@@ -1,5 +1,20 @@
 ﻿using System;
 public partial class EnemyLexerTester
 {
-    
+    public void test_easy1()
+    {
+        assertIsSequenceEqual(@"(", @"(");
+    }
+    public void test_easy2()
+    {
+        assertIsSequenceEqual(@"+", @"+");
+    }
+    public void test_easy3()
+    {
+        assertIsSequenceEqual(@"1", @"int 1");
+    }
+    public void test_easy4()
+    {
+        assertIsSequenceEqual(@"1.23f", @"1.23f");
+    }
 }

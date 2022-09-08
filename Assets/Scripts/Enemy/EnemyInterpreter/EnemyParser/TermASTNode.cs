6e10969cf195d158292baba707999f6342123226
Tomoya Bansho <gku14580@kwansei.ac.jp>
2022-09-08 11:45:20 +0900

    add skeleton classes: DECLRARATION_ST, CALL_ST, BLOCK, EXP_ST, EXP, TERM, FACTOR, NUMBER, REPEAT_ST

---
+++ Assets/Scripts/Enemy/EnemyInterpreter/EnemyParser/TermASTNode.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class TermASTNode : ASTNode
+{
+    public override List<EnemyVM.Instruction> Compile(Dictionary<string, int> vtable)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override string Print(int tab)
+    {
+        throw new NotImplementedException();
+    }
+}

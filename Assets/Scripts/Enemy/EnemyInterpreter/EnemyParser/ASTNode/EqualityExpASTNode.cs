7c90f91e4126e1d845c8ccaa71489dbfbcf304da
Summer498 <h262sawamura@i.softbank.jp>
2022-09-09 18:15:11 +0900

    implement EqualityExpASTNode

--- Assets/Scripts/Enemy/EnemyInterpreter/EnemyParser/ASTNode/EqualityExpASTNode.cs
+++ Assets/Scripts/Enemy/EnemyInterpreter/EnemyParser/ASTNode/EqualityExpASTNode.cs
@@ -3,17 +3,83 @@ using System.Collections.Generic;
 
 public class EqualityExpASTNode : ASTNode
 {
-    public EqualityExpASTNode()
+    private EqualityExpASTNode left;
+    private ScriptToken relationOperator;
+    private RelationalExpASTNode right;
+
+    public EqualityExpASTNode(RelationalExpASTNode relationalExp)
     {
-        throw new NotImplementedException();
+        this.left = null;
+        this.relationOperator = ScriptToken.GenerateToken("", ScriptToken.Type.NONE);
+        this.right = relationalExp;
+    }
+    public EqualityExpASTNode(EqualityExpASTNode left, ScriptToken arithmeticOperator, RelationalExpASTNode right)
+    {
+        this.left = left;
+        this.relationOperator = arithmeticOperator;
+        this.right = right;
+    }
+    public static implicit operator EqualityExpASTNode(RelationalExpASTNode node)
+    {
+        return new EqualityExpASTNode(node);
+    }
+    public static implicit operator EqualityExpASTNode(TermExpASTNode node)
+    {
+        return new EqualityExpASTNode(node);
+    }
+    public static implicit operator EqualityExpASTNode(FactorExpASTNode node)
+    {
+        return new EqualityExpASTNode(node);
+    }
+    public static implicit operator EqualityExpASTNode(UnaryExpASTNode node)
+    {
+        return new EqualityExpASTNode(node);
+    }
+    public static implicit operator EqualityExpASTNode(PrimaryExpASTNode node)
+    {
+        return new EqualityExpASTNode(node);
     }
     public override List<EnemyVM.Instruction> Compile(Dictionary<string, int> vtable)
     {
-        throw new NotImplementedException();
+        var instructions = new List<EnemyVM.Instruction>();
+        if (left != null)
+        {
+            instructions.AddRange(left.Compile(vtable));
+            instructions.AddRange(right.Compile(vtable));
+            switch (relationOperator.type)
+            {
+                case ScriptToken.Type.EQUAL:
+                    instructions.Add(new EnemyVM.Instruction(EnemyVM.Mnemonic.EQ, 2));
+                    break;
+                case ScriptToken.Type.NOT:
+                    instructions.Add(new EnemyVM.Instruction(EnemyVM.Mnemonic.NE, 2));
+                    break;
+            }
+        }
+        else
+        {
+            instructions.AddRange(right.Compile(vtable));
+        }
+        return instructions;
     }
 
     public override string Print(int tab)
     {
-        throw new NotImplementedException();
+        string str = "";
+        if (left != null)
+        {
+            str = left.Print(tab);
+            switch (relationOperator.type)
+            {
+                case ScriptToken.Type.EQUAL:
+                    str += "==";
+                    break;
+                case ScriptToken.Type.NOT:
+                    str += "!=";
+                    break;
+            }
+        }
+        str += right.Print(tab);
+        return str;
     }
 }
--- Assets/Scripts/Enemy/EnemyInterpreter/EnemyParser/ASTNode/Tester/EqualityExpASTNodeTester.cs
+++ Assets/Scripts/Enemy/EnemyInterpreter/EnemyParser/ASTNode/Tester/EqualityExpASTNodeTester.cs
@@ -1,5 +1,60 @@
 using System;
+using UnityEngine.Assertions;
 public partial class EnemyASTNodeTester
 {
+    public void test_EqualityExpASTNode_print1()
+    {
+        var relationOperator1 = ScriptToken.GenerateToken("", ScriptToken.Type.EQUAL);
+        var left = new PrimaryExpASTNode(1);
+        var right = new PrimaryExpASTNode(1);
+        var term = new EqualityExpASTNode(left, relationOperator1, right);
+
+        Assert.AreEqual(term.Print(0), "1==1");
+    }
+
+    public void test_EqualityExpASTNode_compile1()
+    {
+        string[] testCodes = {
+            "PUSH 1",
+            "PUSH 1",
+            "EQ 2"
+        };
+        var relationOperator1 = new ScriptToken();
+        relationOperator1.type = ScriptToken.Type.EQUAL;
+
+        var left = new PrimaryExpASTNode(1);
+        var right = new PrimaryExpASTNode(1);
+        var term = new EqualityExpASTNode(left, relationOperator1, right);
+        checkGeneratedInstructionIsSame(testCodes, term);
+    }
+
+    public void test_EqualityExpASTNode_print2()
+    {
+        var relationOperator1 = new ScriptToken();
+        relationOperator1.type = ScriptToken.Type.NOT;
+
+        var left = new PrimaryExpASTNode(1);
+        var right = new PrimaryExpASTNode(1);
+        var term = new EqualityExpASTNode(left, relationOperator1, right);
+
+        Assert.AreEqual(term.Print(0), "1!=1");
+    }
+
+    public void test_EqualityExpASTNode_compile2()
+    {
+        string[] testCodes = {
+            "PUSH 1",
+            "PUSH 1",
+            "NE 2"
+        };
+        var relationOperator1 = new ScriptToken();
+        relationOperator1.type = ScriptToken.Type.NOT;
+
+        var left = new PrimaryExpASTNode(1);
+        var right = new PrimaryExpASTNode(1);
+        var term = new EqualityExpASTNode(left, relationOperator1, right);
+        checkGeneratedInstructionIsSame(testCodes, term);
+    }
+
 }
 

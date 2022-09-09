17b4215982e74a634aca5c649cf0f15e2ab9318c
Summer498 <h262sawamura@i.softbank.jp>
2022-09-09 22:16:32 +0900

    append test using vm in TermExpASTNodeTester

--- Assets/Scripts/Enemy/EnemyInterpreter/EnemyParser/ASTNode/Tester/TermExpASTNodeTester.cs
+++ Assets/Scripts/Enemy/EnemyInterpreter/EnemyParser/ASTNode/Tester/TermExpASTNodeTester.cs
@@ -3,58 +3,39 @@ using UnityEngine.Assertions;
 
 public partial class EnemyASTNodeTester
 {
-    public void test_TermExpASTNode_print1()
-    {
-        
-        var arithmeticOperator1 = ScriptToken.GenerateToken("", ScriptToken.Type.PLUS);
-        var left = new PrimaryExpASTNode(1);
-        var right = new PrimaryExpASTNode(1);
-        var term = new TermExpASTNode(left, arithmeticOperator1, right);
-
-        Assert.AreEqual(term.Print(0), "1+1");
-    }
-
-    public void test_TermExpASTNode_compile1()
+    public void test_AddTermExpASTNode()
     {
         string[] testCodes = {
-            "PUSH 1",
-            "PUSH 1",
+            "PUSH 13",
+            "PUSH 7",
             "ADD 2"
-        };
-        var arithmeticOperator1 = new ScriptToken();
-        arithmeticOperator1.type = ScriptToken.Type.PLUS;
+        };        
+        var arithmeticOperator1 = ScriptToken.GenerateToken("", ScriptToken.Type.PLUS);
+        var left = new PrimaryExpASTNode(13);
+        var right = new PrimaryExpASTNode(7);
+        var node = new TermExpASTNode(left, arithmeticOperator1, right);
 
-        var left = new PrimaryExpASTNode(1);
-        var right = new PrimaryExpASTNode(1);
-        var term = new TermExpASTNode(left, arithmeticOperator1, right);
-        checkGeneratedInstructionIsSame(testCodes, term);
+        Assert.AreEqual(node.Print(0), "13+7");
+        checkVMReturnValue(node, 13+7);
+        checkGeneratedInstructionIsSame(testCodes, node);
     }
 
-    public void test_TermExpASTNode_print2()
-    {
-        var arithmeticOperator1 = new ScriptToken();
-        arithmeticOperator1.type = ScriptToken.Type.SUB;
-
-        var left = new PrimaryExpASTNode(1);
-        var right = new PrimaryExpASTNode(1);
-        var term = new TermExpASTNode(left, arithmeticOperator1, right);
-
-        Assert.AreEqual(term.Print(0), "1-1");
-    }
-
-    public void test_TermExpASTNode_compile2()
+    public void test_SubTermExpASTNode()
     {
         string[] testCodes = {
-            "PUSH 1",
-            "PUSH 1",
+            "PUSH 42",
+            "PUSH 20",
             "SUB 2"
         };
         var arithmeticOperator1 = new ScriptToken();
         arithmeticOperator1.type = ScriptToken.Type.SUB;
 
-        var left = new PrimaryExpASTNode(1);
-        var right = new PrimaryExpASTNode(1);
-        var term = new TermExpASTNode(left, arithmeticOperator1, right);
-        checkGeneratedInstructionIsSame(testCodes, term);
+        var left = new PrimaryExpASTNode(42);
+        var right = new PrimaryExpASTNode(20);
+        var node = new TermExpASTNode(left, arithmeticOperator1, right);
+
+        Assert.AreEqual(node.Print(0), "42-20");
+        checkVMReturnValue(node, 42-20);
+        checkGeneratedInstructionIsSame(testCodes, node);
     }
 }

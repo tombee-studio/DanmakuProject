25b5a5e4f8ec1c8ccd348be1b0f43685e3901ef6
Summer498 <h262sawamura@i.softbank.jp>
2022-09-09 17:37:17 +0900

    implement primaryExpASTNode

---
+++ Assets/Scripts/Enemy/EnemyInterpreter/EnemyParser/ASTNode/Tester/PrimaryExpASTNodeTester.cs
@@ -0,0 +1,70 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+using System.Linq;
+public partial class EnemyASTNodeTester
+{
+    public void test_intNumberCompile()
+    {
+        int value = 42;
+        string[] testCodes = {
+            $"PUSH {value}"
+        };
+        PrimaryExpASTNode node = new PrimaryExpASTNode(value);
+        checkGeneratedInstructionIsSame(testCodes, node);
+    }
+    public void test_intNumberPrint()
+    {
+        int value = 42;
+        PrimaryExpASTNode node = new PrimaryExpASTNode(value);
+        Assert.AreEqual(node.Print(0), $"{value}");
+    }
+    public void test_floatNumberCompile()
+    {
+        float value = 3.14f;
+        string[] testCodes = {
+            $"PUSH {value}"
+        };
+        PrimaryExpASTNode node = new PrimaryExpASTNode(value);
+        checkGeneratedInstructionIsSame(testCodes, node);
+    }
+    public void test_floatNumberPrint()
+    {
+        float value = 3.14f;
+        PrimaryExpASTNode node = new PrimaryExpASTNode(value);
+        Assert.AreEqual(node.Print(0), $"{value}");
+    }
+    public void test_VariableCompile()
+    {
+        string id = "position";
+        int address = 0;
+        string[] testCodes = {
+            $"PUSH {address}"
+        };
+        PrimaryExpASTNode astNode = new PrimaryExpASTNode(id);
+        var vTable = new Dictionary<string, int>();
+        vTable.Add(id, address);
+        foreach (var instruction in astNode.Compile(vTable)
+            .Zip(testCodes, (i1, i2) => (i1, i2)))
+        {
+            var (i1, i2) = instruction;
+            Assert.AreEqual($"{i1}", i2);
+        }
+    }
+    public void test_VariablePrint()
+    {
+        string id = "position";
+        PrimaryExpASTNode node = new PrimaryExpASTNode(id);
+        Assert.AreEqual(node.Print(0), $"{id}");
+    }
+    public void test_ParenExpCompile()
+    {
+        Debug.LogWarning("test_ParenExpCompile is not implemented yet.");
+    }
+    public void test_ParenExpPrint()
+    {
+        Debug.LogWarning("test_ParenExpPrint is not implemented yet.");
+    }
+}
+

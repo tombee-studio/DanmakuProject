dbd4f568856c7cab028371eb60d0435236035b6e
Summer498 <h262sawamura@i.softbank.jp>
2022-09-09 18:51:30 +0900

    implement ExpASTNode

--- Assets/Scripts/Enemy/EnemyInterpreter/EnemyParser/ASTNode/ExpASTNode.cs
+++ Assets/Scripts/Enemy/EnemyInterpreter/EnemyParser/ASTNode/ExpASTNode.cs
@@ -3,16 +3,40 @@ using System.Collections.Generic;
 
 public class ExpASTNode : ASTNode
 {
-    public ExpASTNode(){
-        throw new NotImplementedException();
+    EqualityExpASTNode equalityExp;
+    public ExpASTNode(EqualityExpASTNode equalityExp){
+        this.equalityExp = equalityExp;
     }
+    public static implicit operator ExpASTNode(EqualityExpASTNode node)
+    {
+        return new ExpASTNode(node);
+    }
+    public static implicit operator ExpASTNode(RelationalExpASTNode node)
+    {
+        return new ExpASTNode(node);
+    }
+    public static implicit operator ExpASTNode(TermExpASTNode node)
+    {
+        return new ExpASTNode(node);
+    }
+    public static implicit operator ExpASTNode(FactorExpASTNode node)
+    {
+        return new ExpASTNode(node);
+    }
+    public static implicit operator ExpASTNode(UnaryExpASTNode node)
+    {
+        return new ExpASTNode(node);
+    }
+    public static implicit operator ExpASTNode(PrimaryExpASTNode node)
+    {
+        return new ExpASTNode(node);
+    }    
     public override List<EnemyVM.Instruction> Compile(Dictionary<string, int> vtable)
     {
-        throw new NotImplementedException();
+        return equalityExp.Compile(vtable);
     }
-
     public override string Print(int tab)
     {
-        throw new NotImplementedException();
+        return equalityExp.Print(tab);
     }
 }
--- Assets/Scripts/Enemy/EnemyInterpreter/EnemyParser/ASTNode/Tester/ExpASTNodeTester.cs
+++ Assets/Scripts/Enemy/EnemyInterpreter/EnemyParser/ASTNode/Tester/ExpASTNodeTester.cs
@@ -1,5 +1,53 @@
 using System;
+using UnityEngine.Assertions;
 public partial class EnemyASTNodeTester
 {
+    void test_ExpASTNode1()
+    {
+        var exp = new ExpASTNode(
+            new EqualityExpASTNode(
+                new RelationalExpASTNode(
+                    new FactorExpASTNode(
+                        new UnaryExpASTNode(ScriptToken.GenerateToken("", ScriptToken.Type.SUB),
+                            new PrimaryExpASTNode(7)
+                        ),
+                        ScriptToken.GenerateToken("", ScriptToken.Type.MULTIPLY),
+                        new PrimaryExpASTNode(
+                            new TermExpASTNode(
+                                new PrimaryExpASTNode(2),
+                                ScriptToken.GenerateToken("", ScriptToken.Type.PLUS),
+                                new PrimaryExpASTNode(3)
+                            )
+                        )
+                    ),
+                    ScriptToken.GenerateToken("", ScriptToken.Type.GREATER_THAN),
+                    new FactorExpASTNode(
+                        new PrimaryExpASTNode(7),
+                        ScriptToken.GenerateToken("", ScriptToken.Type.MULTIPLY),
+                        new PrimaryExpASTNode(13)
+                    )
+                ),
+                ScriptToken.GenerateToken("", ScriptToken.Type.EQUAL),
+                new PrimaryExpASTNode(1)
+            )
+        );
+        string[] testCodes = {
+            "PUSH 7",
+            "PUSH -1",
+            "MUL 2",
+            "PUSH 2",
+            "PUSH 3",
+            "ADD 2",
+            "MUL 2",
+            "PUSH 7",
+            "PUSH 13",
+            "MUL 2",
+            "GT 2",
+            "PUSH 1",
+            "EQ 2",
+        };
+        checkGeneratedInstructionIsSame(testCodes, exp);
+        Assert.AreEqual(exp.Print(0), "-7*(2+3)>7*13==1");
+    }
 }
 

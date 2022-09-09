37876accce83dedde1f12b531b3c349a8e8e636a
Tomoya Bansho <gku14580@kwansei.ac.jp>
2022-09-09 11:58:41 +0900

    add ParseBehavior, ParseBulletAST and ParseActionAST

--- Assets/Scripts/Enemy/EnemyInterpreter/EnemyParser/EnemyParser.cs
+++ Assets/Scripts/Enemy/EnemyInterpreter/EnemyParser/EnemyParser.cs
@@ -8,10 +8,28 @@ public class EnemyParser {
         if (tokens[pos].type != ScriptToken.Type.BEHAVIOR)
             throw new Exception("expected token: behaviour");
         pos++;
-        if (tokens[pos].type != ScriptToken.Type.USER_DEFINED_SYMBOL)
+        if (tokens[pos].type != ScriptToken.Type.SYMBOL_ID)
             throw new Exception("expected token: <ID>");
         string id = tokens[pos].user_defined_symbol;
         pos++;
+        if (tokens[pos].type != ScriptToken.Type.CURLY_BRACKET_LEFT)
+            throw new Exception("expected token: {");
+        pos++;
+        var bulletAST = ParseBulletAST(tokens, ref pos);
+        var actionAST = ParseActionAST(tokens, ref pos);
+        if (tokens[pos].type != ScriptToken.Type.CURLY_BRACKET_RIGHT)
+            throw new Exception("expected token: {");
         return new BehaviourASTNode(id);
+    }
+
+    public BulletASTNode ParseBulletAST(List<ScriptToken> tokens, ref int pos) {
+        pos++;
+        return new BulletASTNode();
+    }
+
+    public ActionASTNode ParseActionAST(List<ScriptToken> tokens, ref int pos)
+    {
+        pos++;
+        return new ActionASTNode();
     }
 }

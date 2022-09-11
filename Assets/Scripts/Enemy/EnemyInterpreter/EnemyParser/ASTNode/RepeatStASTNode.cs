a582acaa7f4a01d2d3b3c415fa77351575436195
Summer498 <h262sawamura@i.softbank.jp>
2022-09-11 17:23:41 +0900

    implement RepeatStASTNode

--- Assets/Scripts/Enemy/EnemyInterpreter/EnemyParser/ASTNode/RepeatStASTNode.cs
+++ Assets/Scripts/Enemy/EnemyInterpreter/EnemyParser/ASTNode/RepeatStASTNode.cs
@@ -1,21 +1,88 @@
 ﻿using System;
 using System.Collections.Generic;
-
+using System.Linq;
+using UnityEngine.Assertions;
 public class RepeatStASTNode : ASTNode
 {
-    ExpASTNode exp;
-    List<StatementASTNode> statements;
-    public RepeatStASTNode(ExpASTNode exp, List<StatementASTNode> statements){
-        this.exp = exp;
-        this.statements = statements;
+    static int loopCount = 0;
+    int N;
+    StatementASTNode statement;
+    public RepeatStASTNode(int numberOfExecutions, StatementASTNode statement)
+    {
+        this.N = numberOfExecutions;
+        this.statement = statement;
+        if (numberOfExecutions < 0) { throw new NotSupportedException("ループ回数を負の値にすることはできません"); }
     }
     public override List<EnemyVM.Instruction> Compile(Dictionary<string, int> vtable)
     {
-        throw new NotImplementedException();
+        // ユーザー定義変数名に @ を登録することはできないことを利用して, 任意のユーザー定義変数名と重複しないループカウンタ変数を定義
+        var counterName = $"@__repeat_loop_counter_{loopCount}__";
+
+        // var i;
+        var counterAddr = vtable.Count;
+        vtable.Add(counterName, counterAddr);
+
+        // i = 0;
+        var assignment = new AssignStASTNode(counterName, new PrimaryExpASTNode(0))  // @__loop_counter__ = 0
+                .Compile(vtable);
+
+        var loopBegin = new List<EnemyVM.Instruction>()
+            // :loop_begin
+            // if (not(i<N)) break;  -> if(not(i<N)) goto :end_loop;
+            .Append(new EnemyVM.Instruction(EnemyVM.Mnemonic.PUSH, counterAddr))
+            .Append(new EnemyVM.Instruction(EnemyVM.Mnemonic.LOAD, 0))
+            .Append(new EnemyVM.Instruction(EnemyVM.Mnemonic.PUSH, N))
+            .Append(new EnemyVM.Instruction(EnemyVM.Mnemonic.LT, 0))
+            .Append(new EnemyVM.Instruction(EnemyVM.Mnemonic.JNE, 0)).ToList();
+
+        var body = statement.Compile(vtable);
+
+        var loopTail = new List<EnemyVM.Instruction>()
+            //i++
+            .Append(new EnemyVM.Instruction(EnemyVM.Mnemonic.PUSH, counterAddr))
+            .Append(new EnemyVM.Instruction(EnemyVM.Mnemonic.LOAD, 0))
+            .Append(new EnemyVM.Instruction(EnemyVM.Mnemonic.PUSH, 1))
+            .Append(new EnemyVM.Instruction(EnemyVM.Mnemonic.ADD, 2))
+            .Append(new EnemyVM.Instruction(EnemyVM.Mnemonic.PUSH, counterAddr))
+            .Append(new EnemyVM.Instruction(EnemyVM.Mnemonic.STORE, 0))
+            //goto :loop_begin
+            .Append(new EnemyVM.Instruction(EnemyVM.Mnemonic.JMP, 0))
+            .ToList();
+        // :end_loop
+
+        loopBegin = loopBegin.Select(e =>
+        {
+            if (e.mnemonic == EnemyVM.Mnemonic.JNE)
+            {
+                return new EnemyVM.Instruction(EnemyVM.Mnemonic.JNE, body.Count + loopTail.Count);
+            }
+            return e;
+        }).ToList();
+        loopTail = loopTail.Select(e =>
+        {
+            if (e.mnemonic == EnemyVM.Mnemonic.JMP)
+            {
+                return new EnemyVM.Instruction(EnemyVM.Mnemonic.JMP, -loopTail.Count - body.Count - loopBegin.Count);
+            }
+            return e;
+        }).ToList();
+
+        // 子 statement 内の break 文から生成された BREAK 命令をループ末尾へのジャンプに置換
+        // 内側ループの BREAK は置換済みであることを想定
+        for (int i = 0; i < body.Count; i++)
+        {
+            if (body[i].mnemonic != EnemyVM.Mnemonic.BREAK) continue;
+            body[i] = new EnemyVM.Instruction(EnemyVM.Mnemonic.JMP, body.Count - i + loopTail.Count);
+        }
+
+        return assignment
+            .Concat(loopBegin)
+            .Concat(body)
+            .Concat(loopTail).ToList();
     }
 
     public override string Print(int tab)
     {
-        throw new NotImplementedException();
+        return GetTabs(tab) + "repeat(" + N + ")" + statement.Print(tab);
     }
 }
--- Assets/Scripts/Enemy/EnemyInterpreter/EnemyParser/ASTNode/Tester/RepeatStASTNodeTester.cs
+++ Assets/Scripts/Enemy/EnemyInterpreter/EnemyParser/ASTNode/Tester/RepeatStASTNodeTester.cs
@@ -1,5 +1,54 @@
 using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
 public partial class EnemyASTNodeTester
 {
+    // TODO: 実行テストは block のテストに書いておく
+    void test_RepeatStASTNode()
+    {
+        int counterAddr = 0;
+        string[] testCodes = {
+            // i = 0
+            $"PUSH {counterAddr}",
+            "PUSH 0",
+            "STORE 0",
+            // :loop_begin
+            // if (not(i<N)) goto :end_loop
+            $"PUSH {counterAddr}",
+            "LOAD 0",
+            "PUSH 100",
+            "LT 0",
+            "JNE 10",
+            // 3+5
+            "PUSH 3",
+            "PUSH 5",
+            "ADD 2",
+            // i++
+            $"PUSH {counterAddr}",
+            "LOAD 0",
+            "PUSH 1",
+            "ADD 2",
+            $"PUSH {counterAddr}",
+            "STORE 0",
+            // goto :loop_begin
+            "JMP -15"
+            // :end_loop
+        };
+        var node = new RepeatStASTNode(100,
+            new TermExpASTNode(
+                new PrimaryExpASTNode(3),
+                ScriptToken.GenerateToken("", ScriptToken.Type.PLUS),
+                new PrimaryExpASTNode(5)
+            )
+        );
+        int i = 0;
+        foreach (var e in node.Compile(new Dictionary<string, int>()))
+        {
+            Debug.Log($"{e}, {i++}");
+        }
+        checkGeneratedInstructionIsSame(testCodes, node);
+        Assert.AreEqual("repeat(100)3+5\n", node.Print(0));
+    }
 }
 

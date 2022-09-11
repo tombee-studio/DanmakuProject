d592827d44b6f22bb56f2e571dae135ad748039e
Summer498 <h262sawamura@i.softbank.jp>
2022-09-11 11:14:22 +0900

    enable StatementASTNode to implicit cast from child tokens.

--- Assets/Scripts/Enemy/EnemyInterpreter/EnemyParser/ASTNode/StatementASTNode.cs
+++ Assets/Scripts/Enemy/EnemyInterpreter/EnemyParser/ASTNode/StatementASTNode.cs
@@ -4,10 +4,41 @@ using System.Collections.Generic;
 public class StatementASTNode : ASTNode
 {
     ASTNode child;
-    public StatementASTNode(ASTNode child)
+    public StatementASTNode(ASTNode statement)
     {
-        this.child = child;
+        this.child = statement;
     }
+    public static implicit operator StatementASTNode(ExpStASTNode node){
+        return new StatementASTNode(node);
+    }
+    public static implicit operator StatementASTNode(ExpASTNode node){
+        return new StatementASTNode(node);
+    }
+    public static implicit operator StatementASTNode(EqualityExpASTNode node)
+    {
+        return new StatementASTNode(node);
+    }
+    public static implicit operator StatementASTNode(RelationalExpASTNode node)
+    {
+        return new StatementASTNode(node);
+    }
+    public static implicit operator StatementASTNode(TermExpASTNode node)
+    {
+        return new StatementASTNode(node);
+    }
+    public static implicit operator StatementASTNode(FactorExpASTNode node)
+    {
+        return new StatementASTNode(node);
+    }
+    public static implicit operator StatementASTNode(UnaryExpASTNode node)
+    {
+        return new StatementASTNode(node);
+    }
+    public static implicit operator StatementASTNode(PrimaryExpASTNode node)
+    {
+        return new StatementASTNode(node);
+    }
+
 
     public override List<EnemyVM.Instruction> Compile(Dictionary<string, int> vtable)
     {

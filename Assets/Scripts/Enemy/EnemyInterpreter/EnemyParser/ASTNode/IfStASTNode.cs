c15911859f11fee6cfea0dfff73b09fec51852f9
Summer498 <h262sawamura@i.softbank.jp>
2022-09-11 11:14:53 +0900

    implement IfStASTNode

--- Assets/Scripts/Enemy/EnemyInterpreter/EnemyParser/ASTNode/IfStASTNode.cs
+++ Assets/Scripts/Enemy/EnemyInterpreter/EnemyParser/ASTNode/IfStASTNode.cs
@@ -1,9 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
-
+using System.Linq;
 public class IfStASTNode : ASTNode
 {
-    ExpStASTNode cond;
+    ExpASTNode cond;
     StatementASTNode ifBody;
     StatementASTNode elseBody;
     public IfStASTNode(
@@ -12,33 +12,51 @@ public class IfStASTNode : ASTNode
         StatementASTNode elseBody
     )
     {
-
+        this.cond = cond;
+        this.ifBody = ifBody;
+        this.elseBody = elseBody;
+    }
+    public IfStASTNode(
+        ExpASTNode cond,
+        StatementASTNode ifBody
+    )
+    {
+        this.cond = cond;
+        this.ifBody = ifBody;
+        this.elseBody = null;
     }
 
     public override List<EnemyVM.Instruction> Compile(Dictionary<string, int> vtable)
     {
-        List<EnemyVM.Instruction> ifBodyInstructions = ifBody.Compile(vtable);
-        int elseAddress = ifBodyInstructions.Count - 1;  // ジャンプ先を相対位置で指定
+        var ifBodyInstructions = ifBody.Compile(vtable);
+        int elseAddress = ifBodyInstructions.Count  // ジャンプ先を相対位置で指定
+            + ((elseBody != null) ? 1 : 0);  // else の直前の JMP を飛び越える
 
-        List<EnemyVM.Instruction> instructions = new List<EnemyVM.Instruction>();
-        instructions.AddRange(cond.Compile(vtable));
-        instructions.Add(new EnemyVM.Instruction(EnemyVM.Mnemonic.PUSH, 0));
-        instructions.Add(new EnemyVM.Instruction(EnemyVM.Mnemonic.JE, elseAddress));
-        instructions.AddRange(ifBody.Compile(vtable));
-        instructions.AddRange(elseBody.Compile(vtable));
-        //TODO:
-        /*
-        stack -> cond
-        PUSH 0
-        JE :else
-        {}
-        :else
-        */
-        throw new NotImplementedException();
+        var instructions
+            = cond.Compile(vtable)
+                .Append(new EnemyVM.Instruction(EnemyVM.Mnemonic.JNE, elseAddress))
+                .Concat(ifBodyInstructions);
+        if (elseBody != null)
+        {
+            var elseBodyInstructions = elseBody.Compile(vtable);
+            int nextAddress = elseBodyInstructions.Count;
+            return instructions
+                .Append(new EnemyVM.Instruction(EnemyVM.Mnemonic.JMP, nextAddress))
+                .Concat(elseBody.Compile(vtable))
+                .ToList();
+        }
+
+        return instructions.ToList();
     }
 
     public override string Print(int tab)
     {
-        throw new NotImplementedException();
+        var original = "if(" + cond.Print(tab) + ")" + ifBody.Print(tab);
+        if (elseBody != null)
+        {
+            return original + " else " + elseBody.Print(tab) + "\n";
+        }
+
+        return original + "\n";
     }
 }
--- Assets/Scripts/Enemy/EnemyInterpreter/EnemyParser/ASTNode/Tester/IfStASTNodeTester.cs
+++ Assets/Scripts/Enemy/EnemyInterpreter/EnemyParser/ASTNode/Tester/IfStASTNodeTester.cs
@@ -1,5 +1,61 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.Assertions;
 public partial class EnemyASTNodeTester
 {
+    void test_ifOnlyIfStASTNodeTester()
+    {
+        PrimitiveValue cond = 1;
+        PrimitiveValue ifBody1 = 2;
+        PrimitiveValue ifBody2 = 3;
+        string[] testCodes = {
+            $"PUSH {cond}",
+            "JNE 3",
+            $"PUSH {ifBody1}",
+            $"PUSH {ifBody2}",
+            "MUL 2",
+        };
+        var node = new IfStASTNode(
+            new PrimaryExpASTNode(cond),
+            new FactorExpASTNode(
+                new PrimaryExpASTNode(ifBody1),
+                ScriptToken.GenerateToken("", ScriptToken.Type.MULTIPLY),
+                new PrimaryExpASTNode(ifBody2)
+            )
+        );
+        checkGeneratedInstructionIsSame(testCodes, node);
+        Assert.AreEqual(node.Print(0), $"if({cond}){ifBody1}*{ifBody2}\n");
+        checkVMReturnValueFromSubProgram(node, ifBody1 * ifBody2);
+    }
+    void test_ifAndElseIfStASTNodeTester()
+    {
+        PrimitiveValue cond = 0;
+        PrimitiveValue ifBody1 = 5;
+        PrimitiveValue ifBody2 = 6;
+        PrimitiveValue elseBody = 7;
+        string[] testCodes = {
+            $"PUSH {cond}",
+            "JNE 4",
+            $"PUSH {ifBody1}",
+            $"PUSH {ifBody2}",
+            "MUL 2",
+            "JMP 1",
+            $"PUSH {elseBody}",
+        };
+        var node = new IfStASTNode(
+            new PrimaryExpASTNode(cond),
+            new FactorExpASTNode(
+                new PrimaryExpASTNode(ifBody1),
+                ScriptToken.GenerateToken("", ScriptToken.Type.MULTIPLY),
+                new PrimaryExpASTNode(ifBody2)
+            ),
+            new PrimaryExpASTNode(elseBody)
+        );
+        checkGeneratedInstructionIsSame(testCodes, node);
+        Assert.AreEqual(node.Print(0), $"if({cond}){ifBody1}*{ifBody2} else {elseBody}\n");
+        checkVMReturnValueFromSubProgram(node, cond != 0 ? ifBody1 * ifBody2 : elseBody);
+    }
 }
 

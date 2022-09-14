12823acd8e8f4ea8afe3eab550177a82f9363c26
Appbird <project.appbird@gmail.com>
2022-09-14 19:00:44 +0900

    define TokenPointer and ParseResult.

--- Assets/Scripts/Enemy/EnemyInterpreter/EnemyParser/EnemyParser.cs
+++ Assets/Scripts/Enemy/EnemyInterpreter/EnemyParser/EnemyParser.cs
@@ -1,10 +1,13 @@
 using System;
-using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
+
 
 /*
  * ロードマップ
+ * 
+ * 各非終端記号に対応したパーサーを定義する
+ * 
+ * 
  * 
  * EnemyParser
  *  
---
+++ Assets/Scripts/Enemy/EnemyInterpreter/EnemyParser/ParseResult.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+public struct ParseResult<N>
+{
+    enum State
+    {
+        Succeeded, Failed
+    };
+    private State _state;
+    private N _parsedNode;
+    private TokenPointer _pointWhenParseCompleted;
+
+
+    public bool IsSucceeded()
+    {
+        return _state == State.Succeeded;
+    }
+    public N ParsedNode
+    {
+        get => IsSucceeded() ? _parsedNode : throw new Exception("This ParseResult is not in Succeeded state, so any result is not contained in this object.");
+    }
+    public TokenPointer pointer
+    {
+        get => IsSucceeded() ? _pointWhenParseCompleted : throw new Exception("This ParseResult is not in Succeeded state, so any result is not contained in this object.");
+    }
+
+
+    public ParseResult(N parsedNode, TokenPointer currentPointer)
+    {
+        _state = State.Succeeded;
+        _parsedNode = parsedNode;
+        _pointWhenParseCompleted = currentPointer;
+    }
+    public static ParseResult<N> Failed() => new() { _state = State.Failed };
+}

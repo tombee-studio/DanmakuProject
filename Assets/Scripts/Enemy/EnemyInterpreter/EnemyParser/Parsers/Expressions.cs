51f7dd03d9c19c8465ea07ab314560cdd8170f45
Appbird <project.appbird@gmail.com>
2022-09-26 11:39:40 +0900

    implements ParseCallFuncStASTNode

--- Assets/Scripts/Enemy/EnemyInterpreter/EnemyParser/Parsers/Expressions.cs
+++ Assets/Scripts/Enemy/EnemyInterpreter/EnemyParser/Parsers/Expressions.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 public partial class EnemyParser
 {
     public ParseResult<CallFuncStASTNode> ParseCallFuncStASTNode(TokenStreamPointer pointer)
@@ -7,8 +8,29 @@ public partial class EnemyParser
             return ParseResult<CallFuncStASTNode>.Failed("This token's line is not function.", "CallFuncStASTNode", pointer);
         var stream = pointer.StartStream();
         stream.should
-            .ExpectSymbolID(out string _tmp)
+            .ExpectSymbolID(out string functionID)
+            .Expect("(");
 
+        var expList = new List<ExpASTNode>();
+        while (
+            stream.maybe
+                .MaybeConsumedBy(ParseExpASTNode, out ExpASTNode expASTNode)
+                .Maybe(",")
+                .IsSatisfied
+        ){ expList.Add(expASTNode); }
+
+        stream.maybe
+            .MaybeConsumedBy(ParseExpASTNode, out ExpASTNode expASTNode1);
+        if (expASTNode1 is ExpASTNode) expList.Add(expASTNode1);
+
+        return new(
+                new CallFuncStASTNode(functionID, expList),
+                stream.CurrentPointer
+        );
+    }
+    public ParseResult<ExpASTNode> ParseExpASTNode(TokenStreamPointer pointer)
+    {
+        throw new NotImplementedException();
     }
     private bool TestCallFuncStASTNode(TokenStreamPointer pointer)
     {

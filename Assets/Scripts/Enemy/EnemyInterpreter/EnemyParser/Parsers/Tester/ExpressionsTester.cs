be4d404e2ee53158abaf6e5f75c97a5399764439
Summer498 <h262sawamura@i.softbank.jp>
2022-09-29 07:12:05 +0900

    append test of ParseUnaryExpASTNode

--- Assets/Scripts/Enemy/EnemyInterpreter/EnemyParser/Parsers/Tester/ExpressionsTester.cs
+++ Assets/Scripts/Enemy/EnemyInterpreter/EnemyParser/Parsers/Tester/ExpressionsTester.cs
@@ -6,31 +6,70 @@ using UnityEngine.Assertions;
 // TODO: EnemyASTNodeTester ではなく, EnemyParserTester ではないか?
 public partial class EnemyParserTester
 {
-    public void ValidatePrintResult(List<ScriptToken> tokens, string expected){
-        var parser = new EnemyParser();
+    public void ValidatePrintResult<N>(
+        List<ScriptToken> tokens,
+        TokenStreamChecker.ParserFunction<N> Parse,
+        string expected
+    ) where N : ASTNode
+    {
         var pointer = new TokenStreamPointer(tokens);
-        var result = parser.ParsePrimaryExpASTNode(pointer);
+        var result = Parse(pointer);
         Assert.AreEqual(expected, result.ParsedNode.Print(0));
+    }
+    public void test_ParseMinus()
+    {
+        var tokens = new List<ScriptToken>()
+            .Append(ScriptToken.GenerateToken("", ScriptToken.Type.SUB))
+            .Append(ScriptToken.GenerateToken("42", ScriptToken.Type.INT_LITERAL))
+            .ToList();
+        ValidatePrintResult(
+            tokens,
+            new EnemyParser().ParseUnaryExpASTNode,
+            "-42"
+        );
+    }
+    public void test_ParseUnary()
+    {
+        var tokens = new List<ScriptToken>()
+            .Append(ScriptToken.GenerateToken("42", ScriptToken.Type.INT_LITERAL))
+            .ToList();
+        ValidatePrintResult(
+            tokens, 
+            new EnemyParser().ParseUnaryExpASTNode,
+            "42"
+        );
     }
     public void test_ParseIntLiteral()
     {
         var tokens = new List<ScriptToken>()
             .Append(ScriptToken.GenerateToken("42", ScriptToken.Type.INT_LITERAL))
             .ToList();
-        ValidatePrintResult(tokens, "42");
+        ValidatePrintResult(
+            tokens,
+            new EnemyParser().ParsePrimaryExpASTNode,
+            "42"
+        );
     }
     public void test_ParseFloatLiteral()
     {
         var tokens = new List<ScriptToken>()
             .Append(ScriptToken.GenerateToken("3.14", ScriptToken.Type.FLOAT_LITERAL))
             .ToList();
-        ValidatePrintResult(tokens, "3.14");
+        ValidatePrintResult(
+            tokens,
+            new EnemyParser().ParsePrimaryExpASTNode,
+            "3.14"
+        );
     }
     public void test_ParseSymbolIDLiteral()
     {
         var tokens = new List<ScriptToken>()
             .Append(ScriptToken.GenerateToken("foo", ScriptToken.Type.SYMBOL_ID))
             .ToList();
-        ValidatePrintResult(tokens, "foo");
+        ValidatePrintResult(
+            tokens,
+            new EnemyParser().ParsePrimaryExpASTNode,
+            "foo"
+        );
     }
 }

a069bddb92413b2cf687737811dcbc1ac0393069
appbird <project.appbird@gmail.com>
2022-10-19 16:19:36 +0900

    Error Message reports position of code where the error happened.

--- Assets/Scripts/Enemy/EnemyInterpreter/EnemyParser/ParserCore/ParseException.cs
+++ Assets/Scripts/Enemy/EnemyInterpreter/EnemyParser/ParserCore/ParseException.cs
@@ -3,6 +3,8 @@
 public class ParseException : Exception
 {
     public ParseException(string message) : base(message) { }
-    public static ParseException Information(string message, TokenStreamPointer pointer)
-        => new(message + $" : at token number {pointer.index}");
+    public static ParseException Information(string message, TokenStreamPointer pointer) {
+        ScriptToken currentToken = pointer.Access();
+        return new(message + $" : at line {currentToken.LineNumber}, column {currentToken.ColumnNumber} token number {pointer.index}");
+    }
 }

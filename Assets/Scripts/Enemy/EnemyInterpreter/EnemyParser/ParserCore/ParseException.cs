cc00af156b0cd6c2ecfc8fe751cdd59930213302
appbird <project.appbird@gmail.com>
2022-10-19 17:26:22 +0900

    ParseException no longer reports token number.

--- Assets/Scripts/Enemy/EnemyInterpreter/EnemyParser/ParserCore/ParseException.cs
+++ Assets/Scripts/Enemy/EnemyInterpreter/EnemyParser/ParserCore/ParseException.cs
@@ -4,14 +4,12 @@ public class ParseException : Exception
 {
     public ParseException(string message) : base(message) { }
     public static ParseException Information(string message, TokenStreamPointer pointer) {
-        TokenRangeInCode range = pointer.Access().range;
-        int endColumnNumber = range.length + range.beginColumnNumber;
-        return new(message + $" : at line {range.lineNumber}, column {range.beginColumnNumber} to {endColumnNumber} token number {pointer.index}");
+        return ParseException.Information(message, pointer.Access());
     }
     public static ParseException Information(string message, ScriptToken scriptToken)
     {
         TokenRangeInCode range = scriptToken.range;
         int endColumnNumber = range.length + range.beginColumnNumber;
-        return new(message + $" : at line {range.lineNumber}, column {range.beginColumnNumber} to {endColumnNumber} token number {pointer.index}");
+        return new(message + $" : at line {range.lineNumber}, column {range.beginColumnNumber} to {endColumnNumber}");
     }
 }
